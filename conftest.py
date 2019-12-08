8b8bc6b567277e78dc2c0ab153ada62169e9f9cc
Cloud Strife <cloudstrife2612@gmail.com>
2019-12-08 22:54:00 +0300

    commit BasePage

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import pytest
-from django.conf.locale import en
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome',
--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,15 @@
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-def test_guest_should_see_login_link(browser):
+def go_to_login_page(browser):
+    link = browser.find_element_by_css_selector("#login_link")
+    link.click()
+
+def test_guest_can_go_to_login_page(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("#login_link")
+    go_to_login_page(browser)
+
+
+#def test_guest_should_see_login_link(browser):
+#    browser.get(link)
+#    browser.find_element_by_css_selector("#login_link")
